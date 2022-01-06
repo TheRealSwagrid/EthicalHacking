0ea45aa23eb25414b55641c80ffffb23969e9679
prochafr <frantz.prochaska@student.uni-augsburg.de>
2022-01-06 16:13:52 +0100

    new changes

---
+++ Andere Internetseiten/The Script Kiddies Are Not Alright Telepolis-Dateien/stub.js
@@ -0,0 +1,1 @@
+!function(e,n,t,m,o){e.IOMmGlobalObject=t,e.IOMm=e.IOMm||function(){(e.IOMm.q=e.IOMm.q||[]).push(arguments)},o="noModule"in n.createElement("script")&&!/Edge/.test(e.navigator.userAgent),e.IOMm.esm=o}(window,document,"IOMm");
